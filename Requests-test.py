fc6d3d3635d1997a11daed63977a4ceb116618b3
Melo.xiang <502850633@qq.com>
2018-01-30 19:54:49 +0800

    爬百度首页

--- Requests-test.py
+++ Requests-test.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-import requests,webbrowser
-#res = requests.get("http://www.baidu.com")
-#print(res.text)
-webbrowser.open("http://www.baidu.com")
+import requests,webbrowser,urllib3,bs4
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://www.baidu.com')
+soup = bs4.BeautifulSoup(r.data,"lxml")
+elements = soup.select('div[class=s_tab]')
+print(elements[0].attrs)
